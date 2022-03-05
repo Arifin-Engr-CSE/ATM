32e8b3c9cb587ce03f1b4361d4e1ada76d008ee7
Shamsul Arifin <Shamsul Arifin@DESKTOP-28D78D2>
2022-03-05 19:09:02 +0600

    initDB

---
+++ ATM.Database/dbo/Stored Procedures/sp_deposit.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sp_deposit]
+	@accNo nvarchar(100),
+	@pin bigint,
+	@amount bigint
+AS
+BEGIN
+	update Customer
+	set Balance=Balance+@amount
+	where AccNo=@accNo and PIN=@pin
+END
---
+++ ATM.Database/dbo/Stored Procedures/sp_withdraw.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE PROCEDURE sp_withdraw
+	@pin bigint,
+	@amount bigint,
+	@accNo nvarchar(100)
+	
+AS
+BEGIN
+	update Customer
+	set Balance=Balance-@amount
+	where AccNo=@accNo and PIN=@pin
+END
